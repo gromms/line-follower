d335d60e2586e5978068fcd8b8fc4ce1b4e1916d
raedovj <raedovj@hotmail.com>
2017-06-05 14:39:58 +0300

    fix pid error calc

--- Code/line-follower/pid.c
+++ Code/line-follower/pid.c
@@ -4,51 +4,39 @@
 #include "pinout.h"
 #include "adc.h"
 
-double Kp = 0.2;
-uint16_t Ki = 0;
-uint16_t Kd = 0.0;
+double Kp = 1;
+//double Ki = 0;
+double Kd = 0.5;
 
-int16_t pError = 0;
-int16_t error = 0;
+double pError = 0;
+double error = 0;
 
-int16_t light_weight[] = {-64,-32,-16,-8,-4,-2,-1,1,2,4,8,16,32,64};
-//int16_t light_weight[] = {-7,-6,-5,-4,-3,-2,-1, 1,2,3,4,5,6,7};
+//int16_t light_weight[] = {-64,-32,-16,-8,-4,-2,-1,1,2,4,8,16,32,64};
+int16_t light_weight[] = {-7,-6,-5,-4,-3,-2,-1, 1,2,3,4,5,6,7};
 
-uint16_t p_light_vals[LIGHTS];
+double prev_light_percent[LIGHTS];
+double light_percent[LIGHTS];
 bool_t is_on_line = TRUE;
 
 void check_losing_line() 
 {
-	// check the leftmost sensors
-	if (light_vals[0] < p_light_vals[0] && light_vals[1] > p_light_vals[1]) 
+	if (prev_light_percent[0] || prev_light_percent[1] || prev_light_percent[2]) 
 	{
-		is_on_line = TRUE;
-		//moving right
-	}
-	else 
-	{
-		//lost line moving left
+		// line lost to the left
 		is_on_line = FALSE;
-		error = light_weight[0];
+		error = light_weight[0] * 100;
 	}
-	
-	// check the rightmost sensors
-	if (light_vals[13] < p_light_vals[13] && light_vals[12] > p_light_vals[12])
+	else if (prev_light_percent[11] || prev_light_percent[12] || prev_light_percent[13]) 
 	{
-		//moving left
-		is_on_line = TRUE;
-	}
-	else 
-	{
-		//lost line moving right
+		// line lost to the right
 		is_on_line = FALSE;
-		error = light_weight[13];
+		error = light_weight[13] * 100;
 	}
 }
 
-int16_t calc_error( uint16_t *lights_max, uint16_t *lights_min)
+double calc_error( uint16_t *lights_max, uint16_t *lights_min)
 {
-	//char buf[16];
+	char buf[16];
 
 	error = 0;
 	double err_sum = 0;
@@ -57,40 +45,48 @@ int16_t calc_error( uint16_t *lights_max, uint16_t *lights_min)
 	
 	for (uint8_t i = 0; i < LIGHTS; i++)
 	{
-
-		double err_percent = ((double)((light_vals[i] - lights_min[i])) / (double)((lights_max[i] - lights_min[i])));			
+		double err_percent = 0;
+		if (light_vals[i] <= lights_min[i] + 150) 
+		{
+			err_percent = 0;
+		}
+		else if (light_vals[i] >= lights_max[i] - 25) 
+		{
+			err_percent = 1;
+		}
+		else 
+		{
+			err_percent = ((double)((light_vals[i] - lights_min[i])) / (double)((lights_max[i] - lights_min[i])));
+		}
 		err_sum += err_percent;	
-		err_percent = err_percent < 0.05 ? 0 : err_percent * 100;
+		
+		light_percent[i] = err_percent;
 	
-		error += (int16_t)(err_percent) * light_weight[i];
-//  		sprintf(buf, "%d | ", (int16_t)err);
-//  		usb_write(buf);
+		error += (int8_t)(err_percent*100) * light_weight[i];
+  		/*sprintf(buf, "%d | ", (int8_t)(err_percent*100));
+  		usb_write(buf);*/
 	}
-	
-	//sprintf(buf, "%d", error);
-	//usb_write(buf);
 	//usb_write("\n");
 	
-	if (error == 0) 
+	if (err_sum == 0) 
 	{
 		if (is_on_line)
 			check_losing_line();
 		else
 			error = pError;
 	}
-	else 
-	{
+	else
 		is_on_line = TRUE;
-	}	
 	
 	for (uint8_t i = 0; i < LIGHTS; i++) 
 	{
-		p_light_vals[i] = light_vals[i];
+		prev_light_percent[i] = light_percent[i];
 	}
-	err_sum = err_sum == 0 ? 1 : err_sum;
+	err_sum = err_sum == 0 ? 1 : err_sum; // avoid division by zero
+	/*sprintf(buf, "%d", (int16_t) ( error / err_sum)  );
+	usb_write(buf);
+	usb_write("\n");*/
 	return error / err_sum;
-
-
 }
 
 int16_t calc_speed(uint16_t *lights_max, uint16_t *lights_min)
@@ -100,5 +96,5 @@ int16_t calc_speed(uint16_t *lights_max, uint16_t *lights_min)
 	int16_t error = calc_error(lights_max, lights_min);
 	PORTB &= ~(1 << STATUS_LED2);
 	
-	return error * Kp + (error - pError) * Kd; 
+	return (int16_t) (error * Kp + (error - pError) * Kd); 
 }
